0c6bda2aade44644fe6c7bb7fdf068f52e91618d
Darshna <darshna.adam@gmail.com>
2022-03-14 20:59:37 +0530

    simple c++ programm using class and object

---
+++ employee.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class employee
+{
+char name[20];
+int age;
+float basic_sal;
+void getInfo()
+{
+    cout<<endl << "Enter name: ";
+    cin >> name;
+    cout<<endl << "Enter age: ";
+    cin >> age;
+    cout<<endl << "Enter basic salary: ";
+    cin >> basic_sal;
+}
+public:
+void displayInfo(){
+    getInfo();
+    cout<<endl << "\tEMPLOYEE INFORMATION\n";
+    cout <<"\t--------------";
+    cout << endl <<"Name: " <<name;
+    cout << endl <<"Age: " <<age;
+    cout << endl <<"Basic salary: " <<basic_sal;
+    cout << endl <<"Gross salary: "<<basic_sal+(0.6*basic_sal)+(0.4*basic_sal);
+}
+};
+
+int main()
+{
+  employee e;
+  e.displayInfo();
+
+    return 0;
+    
+}
